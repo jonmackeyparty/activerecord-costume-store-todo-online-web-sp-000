abdafd82a73f685f852a213190f65dac75f80b57
Jon Mackey <jon@jonmackey.party>
2019-02-06 17:17:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,13 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
+  
   def change 
     create_table costumes do |t|
-      t.name :name 
+      t.string :name
+      t.integer :price 
+      t.string :image_url
+      t.string :size 
+      t.timestamps
+    end
 
 end
 
